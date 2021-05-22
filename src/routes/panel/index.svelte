70bf43dccb6ac6ea1758ccae65ae4339a664eb8e
fabian9799 <fabian9799@protonmail.ch>
2021-05-23 01:46:37 +0200

    Added permissions to panel

--- src/routes/panel/index.svelte
+++ src/routes/panel/index.svelte
@@ -1,42 +1,56 @@
 <script>
-    import { onMount } from "svelte";
-    import { sdk } from "../../appwrite";
-    import Card from "./Card.svelte"
-    let data;
-    onMount(() => {
-        let promise = sdk.database.listDocuments("60a4524fa1134");
+  import { onMount } from "svelte";
+  import { sdk } from "../../appwrite";
+  import Card from "./Card.svelte";
+  let data;
+  onMount(() => {
+    let promise = sdk.teams.list("Admin");
+
+    promise.then(
+      function (response) {
+        if(response.teams[0].name == "Admin"){
+          let promise = sdk.database.listDocuments("60a4524fa1134");
 
         promise.then(
-            function (response) {
-                data = response;
-            },
-            function (error) {
-                console.log(error); // Failure
-            }
+          function (response) {
+            data = response;
+          },
+          function (error) {
+            console.log(error); // Failure
+          }
         );
-    });
+        }
+        
+      },
+      function (error) {
+        console.log(error); // Failure
+      }
+    );
+  });
 </script>
 
 {#if data}
-<section class="text-gray-400 bg-gray-900 body-font">
+  <section class="text-gray-400 bg-gray-900 body-font">
     <div class="container px-5 py-24 mx-auto">
       <div class="flex flex-col text-center w-full mb-20">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Edit documents!</h1>
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+          Edit documents!
+        </h1>
       </div>
       <div class="flex flex-wrap -m-2">
         {#each data.documents as data, i}
-        <Card question={data.question} id={data.$id}/>
+          <Card question={data.question} id={data.$id} />
         {/each}
-        
       </div>
     </div>
   </section>
-    
 {:else}
-<section class="text-gray-400 bg-gray-900 body-font">
+  <section class="text-gray-400 bg-gray-900 body-font">
     <div class="container px-5 py-24 mx-auto">
       <div class="flex flex-col text-center w-full mb-20">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Loading...</h1>
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+          You don't have access to the panel!
+        </h1>
       </div>
     </div>
   </section>{/if}
