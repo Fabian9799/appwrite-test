bd131a6316d2d3410b6b4e15c680790423b1e5f0
fabian9799 <fabian9799@protonmail.ch>
2021-05-22 23:15:14 +0200

    added admin panel (kind of)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,8 +4,8 @@
 
 <script>
 	import { onMount } from "svelte";
-	import {sdk} from "../appwrite"
-	let name = "Please log in!"
+	import { sdk } from "../appwrite";
+	let user;
 
 	onMount(() => {
 		let promise = sdk.account.get();
@@ -13,31 +13,59 @@
 		promise.then(
 			function (response) {
 				console.log(response); // Success
-				name = response.name
+				user = response;
 			},
 			function (error) {
 				console.log(error); // Failure
 			}
 		);
-	})
-
+	});
 
 	function loginDiscord() {
 		sdk.account.createOAuth2Session(
 			"discord",
-			"https://appwrite-test.fabian9799.cloud/",
-			"https://appwrite-test.fabian9799.cloud/"
+			"http://appwrite-test.fabian9799.cloud/",
+			"http://appwrite-test.fabian9799.cloud/"
 		);
 	}
 
+	function logoutDiscord() {
+		let promise = sdk.account.deleteSessions();
+
+		promise.then(
+			function (response) {
+				console.log(response); // Success
+				window.location.reload();
+			},
+			function (error) {
+				console.log(error); // Failure
+			}
+		);
+	}
 </script>
 
 <svelte:head>
 	<title>Discord login</title>
 </svelte:head>
-<h1 class="mx-10 px-3 py-2">{name}</h1>
-<button
-	on:click={loginDiscord}
-	class="mx-10 border-2 border-blue-600 rounded-lg px-3 py-2 text-blue-400 cursor-pointer hover:bg-blue-600 hover:text-blue-200"
-	>Login with Discord</button
->
+
+{#if user}
+	<h1 class="mx-10 px-3 py-2">{user.name}</h1>
+	<a href="/panel">
+		<button
+			class="mx-10 border-2 border-blue-600 rounded-lg px-3 py-2 text-blue-400 cursor-pointer hover:bg-blue-600 hover:text-blue-200"
+			>Access Panel</button
+		></a
+	>
+	<button
+		on:click={logoutDiscord}
+		class="mx-10 border-2 border-red-600 rounded-lg px-3 py-2 text-red-400 cursor-pointer hover:bg-red-600 hover:text-red-200"
+		>Logout</button
+	>
+{:else}
+	<h1 class="mx-10 px-3 py-2">Please Log in!</h1>
+	<button
+		on:click={loginDiscord}
+		class="mx-10 border-2 border-blue-600 rounded-lg px-3 py-2 text-blue-400 cursor-pointer hover:bg-blue-600 hover:text-blue-200"
+		>Login with Discord</button
+	>
+{/if}
---
+++ src/routes/panel/Card.svelte
@@ -0,0 +1,31 @@
+<script>
+    export let question = "No question provided"
+    export let id
+    let buttonText = "DELETE"
+    let status = false
+    let css = "bg-red-500"
+    import { sdk } from "../../appwrite";
+    function deleteEntry() {
+        let promise = sdk.database.deleteDocument('60a4524fa1134', id);
+
+promise.then(function (response) {
+    console.log(response); // Success
+    status = true
+    css = "cursor-not-allowed bg-green-500"
+    buttonText = "Deleted!"
+}, function (error) {
+    console.log(error); // Failure
+    alert("Dude you don't have permission")
+});
+    }
+</script>
+
+
+<div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+    <div class="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+      <div class="flex-grow">
+        <h2 class="text-white title-font font-medium">{question}</h2>
+          <button on:click={deleteEntry} class="{css} inline-flex mt-4 text-white border-0 py-2 px-6 focus:outline-none w-full rounded text-lg" disabled={status}>{buttonText}</button>
+      </div>
+    </div>
+  </div>
